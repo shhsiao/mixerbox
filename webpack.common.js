7d81eb0a293a54367ec62da5bca681cb52da18cf
Sam Hsiao <shenghanhsiao@gmail.com>
2021-09-10 15:31:04 +0800

    add univerial alias and css module setting

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   output: {
@@ -24,14 +25,37 @@ export default {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(path.resolve(), 'src'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
