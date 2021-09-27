1a897e4023287c1a6964f293e69633af60b4b3b9
Sam Hsiao <samhsiao@pilottv.com.tw>
2021-09-27 16:36:34 +0800

    fix footer

--- src/mixerbox/Content/index.tsx
+++ src/mixerbox/Content/index.tsx
@@ -1,28 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
-const Content = ({selected})=>{
-    const [data, setData] = useState([]);
-   useEffect(()=>{
-    const fetchContent =()=>{
-        return fetch('https://newsapi.org/v2/top-headlines?country=tw&apiKey=c14ac1132c5b4fabac7ee0bb1818ea79').then(d=>d.json()).then(v=>{
-            console.log(v);
-            setData(v.articles);
-        }) 
-    }
+const Content = ({ selected }) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchContent = () => {
+      return fetch(
+        'https://newsapi.org/v2/top-headlines?country=tw&apiKey=c14ac1132c5b4fabac7ee0bb1818ea79'
+      )
+        .then((d) => d.json())
+        .then((v) => {
+          console.log(v);
+          setData(v.articles);
+        });
+    };
     fetchContent();
-   },[])
-return <div>
-<div>{selected}</div>
-<div className={`${styles.grid} grid`}> 
-
-{data.map((v,i)=>{
-return <div key={v.content+i}>
-    <img src={v.urlToImage} height="150"/>
-    <div className={styles.title}>{v.title}</div>
-    <div className={styles.author}>{v.author}</div>
-</div>
-})}</div>
-</div>
-}
+  }, []);
+  return (
+    <div className="overflow">
+      <div>{selected}</div>
+      <div className={`${styles.grid} grid`}>
+        {data.map((v, i) => {
+          return (
+            <div key={v.content + i}>
+              <img src={v.urlToImage} height="150" />
+              <div className={styles.title}>{v.title}</div>
+              <div className={styles.author}>{v.author}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default Content;
