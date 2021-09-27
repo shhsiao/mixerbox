4485f42def735fdd0c47667c4485c35df50fb301
Sam Hsiao <samhsiao@pilottv.com.tw>
2021-09-27 17:05:54 +0800

    add material icon

--- src/mixerbox/Header/index.tsx
+++ src/mixerbox/Header/index.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React from 'react';
 import styles from './styles.module.css';
+import SearchIcon from '@material-ui/icons/Search';
 
-const Header = ()=>{
-    return (
-        <>
-        <div className={styles.wrapper} >
-            <img src="https://1000logos.net/wp-content/uploads/2017/12/Bing-logo.png" height="50" width="150"/>
-            <input type="string" className={styles.search}/>
+const Header = () => {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <img
+          src="https://1000logos.net/wp-content/uploads/2017/12/Bing-logo.png"
+          height="50"
+          width="150"
+        />
+        <div className={styles.search}>
+          <input type="text" className={styles.input} />
+          <div className={styles.icon}>
+            <SearchIcon color="primary" />
+          </div>
         </div>
-        <div className={styles.line} />
-        </>
-    )
-}
-export default Header
+      </div>
+      <div className={styles.line} />
+    </>
+  );
+};
+export default Header;
